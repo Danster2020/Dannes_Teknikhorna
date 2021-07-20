973c9d1d1f93332e63f4c68c40fb4e4f77bd3d63
Danster2020 <daniel.bleckert@me.com>
2021-07-20 22:21:04 +0200

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,7 @@ resizeTimer = setTimeout(() => {
 }, 400);
 });
 
-// When user scrolls into view
-const observer = new IntersectionObserver(entries => {
-  // Loop over the entries
-  entries.forEach(entry => {
-    // If the element is visible
-    if (entry.isIntersecting) {
-      // Add the animation class
-      entry.target.classList.add('animate-fadeInUp');
-    }
-  });
-});
+
 observer.observe(document.querySelector('#img_grid'));
 
 function showExitIcon() {
