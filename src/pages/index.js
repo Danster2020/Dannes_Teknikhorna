08c150044bd5a04ffb7195c6ec6aaa2b5f01b6cf
Danster2020 <daniel.bleckert@me.com>
2024-02-18 15:00:02 +0100

    decap identity widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -86,6 +86,11 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => (
+  <>
+    <Seo title="Home" />
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+  </>
+)
 
 export default IndexPage
