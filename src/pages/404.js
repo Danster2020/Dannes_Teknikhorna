28f69bc263879fd65e40f574a455f1ab4682b9c2
Danster2020 <daniel.bleckert@me.com>
2024-03-30 23:51:07 +0100

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,19 @@
 import * as React from "react"
+import { Link } from 'gatsby';
 
 import Seo from "../components/seo"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
 const NotFoundPage = () => (
     <>
-        <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Header></Header>
+        <div className="flex flex-col items-center min-h-screen">
+            <h1 className="text-5xl mt-40">404: Not Found</h1>
+            <p className="text-lg mt-10">Sidan kunde tyvärr inte hittas.</p>
+            <Link className="bg-def_purple_2 text-white px-8 py-4 mt-10 rounded-lg" to="/">Gå till Hem</Link>
+        </div>
+    <Footer></Footer>
     </>
 )
 
